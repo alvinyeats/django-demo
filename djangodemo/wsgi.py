faabf185546b317f0767649cd3ccf5ed0ad6d14e
alvin <alvinyeats@gmail.com>
2019-01-27 14:07:37 +0800

    update project name

--- djangodemo/wsgi.py
+++ djangodemo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autht.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangodemo.settings')
 
 application = get_wsgi_application()
